344572fb65def063e59d92499d9c2c611d9ef36d
Olgavav <petrova-olga@myrambler.ru>
2022-09-29 18:24:33 +0400

    3 задание

---
+++ Task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Введите число обозначающее день недели: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number > 7)
+      {
+        Console.WriteLine("Такого дня недели нет");
+      }
+if (number < 1)
+      {
+        Console.WriteLine("Такого дня недели нет");
+      }
+else
+if (number == 1)
+{
+        Console.WriteLine("Рабочий день");
+      }
+ if (number == 2)
+{
+        Console.WriteLine("Рабочий день");
+      }     
+if (number == 3)
+{
+        Console.WriteLine("Рабочий день");
+      }  
+if (number == 4)
+{
+        Console.WriteLine("Рабочий день");
+      }  
+if (number == 5)
+{
+        Console.WriteLine("Рабочий день");
+      }  
+if (number == 6)
+{
+        Console.WriteLine("Выходной");
+      }  
+if (number == 7)
+{
+        Console.WriteLine("Выходной");
+      }  
