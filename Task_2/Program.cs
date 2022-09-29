1fada6bc57728c671a9a0ef8d59b54bfc5c42f5d
Olgavav <petrova-olga@myrambler.ru>
2022-09-29 17:52:48 +0400

    2 задание

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = (int)Math.Abs(int.Parse(Console.ReadLine()!));
+if (number < 100)
+      {
+        Console.WriteLine("Третьей цифры нет");
+      }
+else
+{
+    Console.WriteLine(number.ToString()[2]);
+}
